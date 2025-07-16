f6ad9af1175ef3334539c4f6405cb0a5ebb6bdf2
Timothy Gilbert <tsgilbert35@hotmail.com>
2025-07-16 11:14:33 -0700

    added clean up and exporting to folders dir (by state)

--- html_js_scrape.R
+++ html_js_scrape.R
@@ -10,16 +10,19 @@ library(httr)
 library(purrr)
 library(dplyr)
 
-scrape_leaders <- function(loc_page_url,
+
+scrape_leaders <- function(loc_page_url, location_data,
                            hero_sel    = ".kw-locationHero-content__grid-row1 a",
                            avatar_sel  = ".AvatarCard-bio-full-name",
                            timeout     = 30,
-                           poll_delay  = 2) {
+                           poll_delay  = 3) {
   # # testing
   # loc_page_url <- "https://locations.kw.com/location/10"
   
   b <- ChromoteSession$new()
   on.exit(b$close(), add = TRUE)
+  
+  Sys.sleep(0.2)
   
   # Enable Chrome DevTools domains
   b$Page$enable()
@@ -28,6 +31,9 @@ scrape_leaders <- function(loc_page_url,
   
   # 1) Navigate to the location page & wait for the hero link
   b$Page$navigate(loc_page_url)
+  
+  Sys.sleep(0.2)
+  
   start <- Sys.time()
   repeat {
     ready <- b$Runtime$evaluate(
@@ -39,6 +45,8 @@ scrape_leaders <- function(loc_page_url,
     }
     Sys.sleep(poll_delay)
   }
+  
+  Sys.sleep(1)
   
   # 2) Grab the base URL from the hero link
   js_fetch_hero <- sprintf(
@@ -67,6 +75,8 @@ scrape_leaders <- function(loc_page_url,
   # if (httr::status_code(httr::HEAD(leaders_url, timeout(5))) != 200) {
   #   return(tibble())  # no leaders page
   # }
+  
+  Sys.sleep(0.2)
   
   # 4) Navigate to the leaders page & wait for avatars
   b$Page$navigate(leaders_url)
@@ -119,37 +129,88 @@ scrape_leaders <- function(loc_page_url,
   }
   
   phones <- numbers[grep("tel:",numbers)]
-  email <- numbers[grep("mailto:",numbers)]
+  emails <- numbers[grep("mailto:",numbers)]
 
   # get rid of extra contacts
   num_wanted <- length(phones)-1
   
-  # final clean up
+  # Final clean up -->
   phones <- phones %>% 
     sub("^tel:", "", .) %>%    # "tel:512.699.3425"
     gsub("\\D", "", .)         # "5126993425"
+  # Pad 10-digit numbers with leading "1"
+  phones_clean <- ifelse(nchar(phones) == 10, paste0("1", phones), phones)
   
-  #b$close()
+  emails_clean <- emails %>% 
+    sub("^mailto:", "", .)
   
+  # Dealing with strange symbols and characters
+  titles_cleaned <- titles %>%
+    iconv(from = "UTF-8", to = "ASCII//TRANSLIT") %>%               # fix encoding
+    str_remove_all("[?!#$%^]") %>%                                  # remove listed symbols
+    str_squish()
+  
+  # # if there are contacts
+  # if (length(first_name) > 1) {
+  #   
+  # }
+
   # 7) Return as tibble
   data <- tibble(
-    `First Name`      = full_names[1:num_wanted],
+    `First Name`      = first_name[1:num_wanted],
     `Last Name`       = last_name[1:num_wanted],
-    `Email`           = emails[1:num_wanted],
-    `Phone Number`    = phones[1:num_wanted],
+    `Email`           = emails_clean[1:num_wanted],
+    `Phone Number`    = phones_clean[1:num_wanted],
     `Company Name`    = sub("^https?://([^\\.]+)\\.kw\\.com.*$", "\\1", base_site),
     `Lead Status`     = "New",
     `Lifecycle Stage` = "Lead",
     `Buying Role`     = "",
     `Contact owner`   = "Andrew Gilbert",
-    `Job title`       = titles[1:num_wanted]
+    `Job title`       = titles_cleaned[1:num_wanted]
     #ProfileLink       = links,
     #Source            = leaders_url,
   )
-  t <- str_trim(substr(Sys.time(),1,19))
-  code <- gsub(":","_",t)
-  code <- gsub(" ","_",code)
   
+  # Checking location data for url to place in correct folder dir
+  site <- location_data[which(location_data$url == loc_page_url),]
+  site_trim <- str_trim(sub("\\d+", "", site$State))
+  site_folder <- gsub(", ", "_", site_trim)
+
+  # Save and sort in directory
   office <- sub("^https?://([^\\.]+)\\.kw\\.com.*$", "\\1", base_site)
-  write.csv(data, paste0("kw_data/contacts_",office,"_",code,".csv"))
+  office_clean_raw <- office %>% 
+    str_remove_all("[?!#$%^]") %>%                                  # remove listed symbols
+    str_squish()
+  office_clean <- gsub("\\.","_",office_clean_raw)
+  # for special case
+  office_clean <- sub("https://","",office_clean)
+  office_clean <- gsub("/","",office_clean)
+  
+  state_code <- substr(site_folder,nchar(site_folder)-1,nchar(site_folder))
+  
+  # Check if both characters are uppercase A-Z for state - make state folder -->
+  if (grepl("^[A-Z]{2}$", state_code)) {
+    # Create folder if it doesn't exist
+    if (!dir.exists(paste0("kw_data/",state_code))) {
+      dir.create(paste0("kw_data/",state_code), recursive = TRUE)
+    }
+    # Then create sub-folder for site
+    if (!dir.exists(paste0("kw_data/",state_code,"/",site_folder))) {
+      dir.create(paste0("kw_data/",state_code,"/",site_folder), recursive = TRUE)
+    }
+    write.csv(data, paste0("kw_data/",state_code,"/",site_folder,"/contacts_",office_clean,".csv"))
+    # Else no state to put in folder -->
+  } else {
+    # Create folder if it doesn't exist - no state found - create folder for location
+    if (!dir.exists(paste0("kw_data/",site_folder))) {
+      dir.create(paste0("kw_data/",site_folder), recursive = TRUE)
+    }
+    write.csv(data, paste0("kw_data/",site_folder,"/contacts_",office_clean,".csv"))
+  }
+  
 }
+
+# t <- str_trim(substr(Sys.time(),1,19))
+# code <- gsub(":","_",t)
+# code <- gsub(" ","_",code)
+# write.csv(data, paste0("kw_data/contacts_",office,"_",code,".csv"))
